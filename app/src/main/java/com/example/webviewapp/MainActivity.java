0e93713c880c6640dd49abdff60ab522920f9d62
thummabalraj <36445941+thummabalraj@users.noreply.github.com>
2020-06-08 14:51:41 +0530

    Initial Master Version

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,217 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.PermissionRequest;
+import android.webkit.URLUtil;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+    private String webUrl = "https://pixabay.com/";
+
+    //private String webUrl = getResources().getString(R.string.web_url);
+
+    WebView webview;
+    RelativeLayout relativeLayout;
+    SwipeRefreshLayout swipeRefreshLayout;
+    Button btnNoInternetConnection;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Hide ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        //Display FULLSCREEN
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        btnNoInternetConnection = (Button) findViewById(R.id.btnNoConnection);
+        btnNoInternetConnection.setOnClickListener(btnClickListener);
+
+        //Set webview
+        webview = (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new AppWebViewClient());
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setColorSchemeColors(Color.BLUE, Color.YELLOW, Color.GREEN);
+        //Implement Refresh Listener event for swipe
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (swipeRefreshLayout.isRefreshing()) {
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+                webview.reload();
+            }
+        });
+
+        if (savedInstanceState != null) {
+            webview.restoreState(savedInstanceState);
+        } else {
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.getSettings().setLoadWithOverviewMode(true);
+            webview.getSettings().setUseWideViewPort(true);
+            webview.getSettings().setDomStorageEnabled(true);
+            webview.getSettings().setLoadsImagesAutomatically(true);
+            webview.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
+            checkConnectivity();
+        }
+
+        webview.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(final String s, final String s1, final String s2, final String s3, long l) {
+                Dexter.withContext(MainActivity.this)
+                        .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(s));
+                                request.setMimeType(s3);
+                                String cookies = CookieManager.getInstance().getCookie(s);
+                                request.addRequestHeader("cookie",cookies);
+                                request.addRequestHeader("User-Agent",s1);
+                                request.setDescription("Downloading File.....");
+                                request.setTitle(URLUtil.guessFileName(s,s2,s3));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalPublicDir(
+                                        Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                                                s,s2,s3));
+                                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                                downloadManager.enqueue(request);
+                                Toast.makeText(MainActivity.this, "Downloading File..", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+
+        //Solved WebView SwipeUp Problem
+        webview.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                if (webview.getScrollY() == 0) {
+                    swipeRefreshLayout.setEnabled(true);
+                } else {
+                    swipeRefreshLayout.setEnabled(false);
+                }
+            }
+        });
+
+    }
+
+    //Display pages in webview client
+    private class AppWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            setTitle(view.getTitle());
+            return true;
+
+//            Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//            startActivity(intent);
+//            return true;
+        }
+    }
+
+    //Handle Key event like back and forward
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack();
+            return true;
+        }
+
+        if ((keyCode == KeyEvent.KEYCODE_FORWARD) && webview.canGoForward()) {
+            webview.goForward();
+            return true;
+        }
+
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void checkConnectivity() {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileNetwork = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if (wifi.isConnected()) {
+            webview.loadUrl(webUrl);
+            webview.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+        } else if (mobileNetwork.isConnected()) {
+            webview.loadUrl(webUrl);
+            webview.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+        } else {
+            webview.setVisibility(View.GONE);
+            relativeLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    // Create an anonymous implementation of OnClickListener
+    private View.OnClickListener btnClickListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            checkConnectivity();
+        }
+    };
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webview.saveState(outState);
+    }
+
+}
